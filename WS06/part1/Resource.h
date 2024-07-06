41f9ad02072755e08ae9abc72134901d3b3dae16
cherylchao6 <nmpyt21@gmail.com>
2024-07-06 14:56:29 -0400

    uploaded part1 changing Directory

--- WS06/part1/Resource.h
+++ WS06/part1/Resource.h
@@ -2,28 +2,25 @@
 
 #ifndef SENECA_RESOURCE_H
 #define SENECA_RESOURCE_H
-
+#include "Flags.h"
 #include <iostream>
 #include <string>
-#include "Flags.h"
 
 namespace seneca {
-    class Resource {
-    protected:
-        // Stores the name of the resource
-        std::string m_name{};
-        // Stores the absolute path of the folder where the resource is located
-        std::string m_parent_path = "/";
+class Resource {
+protected:
+  std::string m_name{};
+  std::string m_parent_path{};
 
-    public:
-        virtual void update_parent_path(const std::string&) = 0;
-        virtual std::string name() const = 0;
-        virtual int count() const = 0;
-        virtual std::string path() const = 0;
-        virtual size_t size() const = 0;
-        virtual NodeType type() const = 0;
-        virtual ~Resource() = default;
-    };
-}  // namespace seneca
+public:
+  virtual void update_parent_path(const std::string &path) = 0;
+  virtual std::string name() const = 0;
+  virtual int count() const = 0;
+  virtual std::string path() const = 0;
+  virtual size_t size() const = 0;
+  virtual NodeType type() const = 0;
+  virtual ~Resource() = default;
+};
+} // namespace seneca
 
 #endif
